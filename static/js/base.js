bde64909d50b3b9c31c844e073e6113979c84946
Abhinav Gyawali <104615184+abhinav0014@users.noreply.github.com>
2023-02-09 09:09:45 +0545

    Add files via upload

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,5 @@
 const toggle = document.getElementById('toggledark');
-
+const pi = document.getElementById('profilei');
 
 if(localStorage.getItem('theme')==null){
 localStorage.setItem('theme','light');
@@ -9,9 +9,11 @@ let localData = localStorage.getItem('theme');
 if (localData=='light'){
     toggle.className = "fa-solid fa-moon";  
     document.body.classList.remove('dark-theme');
+    pi.src="https://t3.gstatic.com/images?q=tbn:ANd9GcQ-aUqsd9xh5Py3rBFvYT09npTJ3IzfBMzoxOjI8wXeMJzTmeK9";
 }else if (localData=='dark'){
     toggle.className = "fa-regular fa-sun";  
     document.body.classList.add('dark-theme');
+    pi.src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png";
 }
 toggle.addEventListener('click', function () {
     document.body.classList.toggle("dark-theme");
@@ -21,9 +23,9 @@ toggle.addEventListener('click', function () {
     } else {
         toggle.className = "fa-solid fa-moon";
         localStorage.setItem('theme','light');
+        pi.src="https://t3.gstatic.com/images?q=tbn:ANd9GcQ-aUqsd9xh5Py3rBFvYT09npTJ3IzfBMzoxOjI8wXeMJzTmeK9";
     }
 });
 
 console.log('hehheheheheheheh what are u doin!');
-
 
